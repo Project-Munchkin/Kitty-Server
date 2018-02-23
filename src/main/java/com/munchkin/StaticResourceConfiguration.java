5cf6ae6546d8e1e8e5d5ef8f01944abf368822e3
leemiyoung <leemy0102@naver.com>
2018-02-23 13:06:40 +0900

    resource 경로 수정

--- src/main/java/com/munchkin/StaticResourceConfiguration.java
+++ src/main/java/com/munchkin/StaticResourceConfiguration.java
@@ -1,19 +1,24 @@
 package com.munchkin;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-
+@EnableWebMvc
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
-	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { 
-		      "classpath:/com/munchkin/image/uniqlo/"};
-		  
-	@Override
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/**")
                 .addResourceLocations("classpath:com/munchkin/");
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
